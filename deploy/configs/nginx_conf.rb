ee9f540c1cd98cf98b471bddd47be5daa75a9131
David Coallier <dcoallier@engineyard.com>
2014-02-10 16:22:15 +0000

    Added a name

--- deploy/configs/nginx_conf.rb
+++ deploy/configs/nginx_conf.rb
@@ -1,7 +1,7 @@
 nginx_block =<<eos
 server {
     listen      80;
-    server_name  _;
+    server_name abuseengine.engineyard.com;
     
     error_log /var/log/nginx/appname-error.log;
     access_log /var/log/nginx/appname-access.log;
